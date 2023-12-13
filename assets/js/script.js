2df0b0994bb90a4372338893a8046e7ea35e9dfe
EdoBergamo <hello@amtriix.it>
2023-12-13 06:12:10 +0000

    [FEAT] Milestone 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,18 @@ createApp({
         { text: 'Asda', done: false },
         { text: 'Asdaasd', done: true },
         { text: 'Asdadsadas', done: false },
-      ]
+      ],
+      newTodoText: '',
     }
   },
   methods: {
-    removeTodo(index) {
+    createTodo() {
+      if (this.newTodoText.trim() !== '') {
+        this.todos.push({ text: this.newTodoText, done: false })
+        this.newTodoText = ''
+      }
+    },
+    deleteTodo(index) {
       this.todos.splice(index, 1)
     }
   }
